044d62e06a243f91958668155d4f174099619aa9
atharva <atharva.ek@gmail.com>
2021-08-12 17:05:06 +0530

    Created a build version of react app, some changes in package.json of server.

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const path = require("path");
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname, "/client/build")));
 const port = 5000;
 app.listen(process.env.PORT || port, ()=> console.log("Server started on port " + port));
 
@@ -17,6 +18,10 @@ const userSchema = new mongoose.Schema({
 });
 
 const User = mongoose.model("User", userSchema);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build'))
+})
 
 app.get("/", function(req, res){
     console.log("inside /");
